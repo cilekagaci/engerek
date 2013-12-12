7f3920c0dbd38af69db5a4bc73a834deb00fd952
amac <amac@herdagdelen.com>
2013-12-11 20:56:55 -0800

    Document stemmer and deasciifier

--- src/engerek/balkans.py
+++ src/engerek/balkans.py
@@ -7,10 +7,17 @@ TurkishDeasciifier = jnius.autoclass('turkish.Deasciifier')
 
 
 class BalkansDeasciifier(Deasciifier):
+    """A wrapper class that uses Balkan's Deasciifier"""
 
     def __init__(self):
         self.deasciifier = TurkishDeasciifier()
 
     def deasciify(self, text):
+        """Returns deasciified version of a Turkish text.
+
+        :param unicode text: ASCII text to be deasciified
+        :returns unicode: Deasciified text
+        """
+
         self.deasciifier.setAsciiString(text)
         return self.deasciifier.convertToTurkish().decode('utf-8')
