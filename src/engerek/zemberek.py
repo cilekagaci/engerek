757ac3155f8a08fc6f389865a19b4aa3c421e2cd
amac <amac@herdagdelen.com>
2013-12-11 20:56:55 -0800

    Document stemmer and deasciifier

--- src/engerek/zemberek.py
+++ src/engerek/zemberek.py
@@ -9,14 +9,25 @@ Zemberek = jnius.autoclass('net.zemberek.erisim.Zemberek')
 
 
 class ZemberekStemmer(Stemmer):
+    """Wrapper class for Zemberek's kokBulucu (Root finder)"""
+
     LANGUAGE = TurkiyeTurkcesi()
     ZEMBEREK = Zemberek(LANGUAGE)
     ROOT_FINDER = ZEMBEREK.kokBulucu()
 
     def __init__(self, prefer_infinitive_form=False):
+        #: If prefer_infinitive_form is set to true verb stems will be
+        #: returned in infinitive form
         self.prefer_infinitive_form = prefer_infinitive_form
 
     def stem(self, word):
+        """Finds the stem of a word.
+
+        :param unicode word: The word to be stemmed
+        :returns list: A list of unicode strings which contain possible
+        stems of the word
+        """
+
         roots = self.ROOT_FINDER.kokBul(word)
 
         if len(roots) == 0:
